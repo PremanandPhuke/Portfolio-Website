881037225ea3307deb1ec6054aa0d61480e16008
Premanand Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2023-01-01 22:40:26 +0530

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,23 @@
+// let btn1= document.getElementById('refresh1')
+// let btn2= document.getElementById('refresh2')
+// let btn3= document.getElementById('refresh3')
+// let btn4= document.getElementById('refresh4')
+// let btn5= document.getElementById('refresh5')
+
+
+
+// btn1.addEventListener('click', ()=>{
+//     window.location.reload();
+// })
+// btn2.addEventListener('click', ()=>{
+//     window.location.reload();
+// })
+// btn3.addEventListener('click', ()=>{
+//     window.location.reload();
+// })
+// btn4.addEventListener('click', ()=>{
+//     window.location.reload();
+// })
+// btn5.addEventListener('click', ()=>{
+//     window.location.reload();
+// })
